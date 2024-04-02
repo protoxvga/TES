c591305ef9a598dc1f089b75ded5241079cda1b0
Utilisateur <pierre.perrin@epitech.eu>
2024-04-02 14:13:26 +0200

    [FIX] - folder structure

--- tes-web/resources/js/Components/ApplicationLogo.tsx
+++
@@ -1,9 +0,0 @@
-export default function ApplicationLogo() {
-    return (
-        <img
-            className="w-28 h-28"
-            src="/storage/images/tiimeLogo.png"
-            alt="image"
-        />
-    );
-}

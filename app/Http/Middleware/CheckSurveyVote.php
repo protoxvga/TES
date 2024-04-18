5b14150154d21a6c93fd60df64b723edbae6ae03
Pierre Perrin <63340473+protoxvga@users.noreply.github.com>
2024-04-18 11:08:06 +0200

    [FIX] Opening hour of votes (#19)

--- app/Http/Middleware/CheckSurveyVote.php
+++ app/Http/Middleware/CheckSurveyVote.php
@@ -10,15 +10,12 @@ class CheckSurveyVote
     public function handle(Request $request, Closure $next)
     {
         $now = now();
-        // Check if it's a weekday (Monday to Friday)
         if ($now->isWeekday()) {
-            // Check if the current time is within the allowed range (10 to 13:00)
-            if ($now->hour >= 10 && $now->hour < 13) {
+            if ($now->hour >= 9 && $now->hour < 13) {
                 return $next($request);
             }
         }
 
-        // Redirect back with error message if not within allowed range or not a weekday
-        return redirect()->back()->withErrors(['message' => 'Tu ne peux pas voter en dehors des horaires de vote (10h - 13h00, du lundi au vendredi).']);
+        return redirect()->back()->withErrors(['message' => 'Tu ne peux pas voter en dehors des horaires de vote (9h00 - 13h00, du lundi au vendredi).']);
     }
 }
